9e120cc1748815cfd320b32ce48b4ae3532314e1
averyden <igo408gt@gmail.com>
2025-02-11 12:49:50 +0100

    create properties and handle the information form em

--- fjernfyn/ViewModels/LoginViewModel.cs
+++ fjernfyn/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using fjernfyn.Repositories;
+using Microsoft.Extensions.Configuration;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
@@ -9,23 +10,41 @@ namespace fjernfyn
     public class LoginViewModel : INotifyPropertyChanged
     {
         private readonly string con;
+
+        private EmployeeRepository empRepo = new EmployeeRepository();
+
         
         public ICommand loginCommand { get; }
 
+        private string _userName;
 
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; OnPropertyChanged(nameof(UserName)); }
+        }
+
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(nameof(Password)); }
+        }
 
         public LoginViewModel() 
         {
             loginCommand = new CommandHandler(OnLoginClicked);
-            
-         
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
         private void OnLoginClicked() 
         {
-            
+            empRepo.HandleInformation(UserName, Password);
+
+            // Should it do anything if it returns true/false or should we EXCLUSIVELY handle it in the repository.
         }
 
         protected void OnPropertyChanged(string propertyName)
