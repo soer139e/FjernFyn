ee6ad23727659d91108f60cf53ac0e54db0e0cb1
averyden <igo408gt@gmail.com>
2025-02-11 13:07:47 +0100

    change name of field

--- fjernfyn/Repositories/EmployeeRepository.cs
+++ fjernfyn/Repositories/EmployeeRepository.cs
@@ -1,25 +1,33 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace fjernfyn.Repositories
 {
     internal class EmployeeRepository
     {
-        private readonly string con;
+        private readonly string conString;
         public EmployeeRepository() 
         {
             IConfigurationRoot config = new ConfigurationBuilder()
              .AddJsonFile("appsettings.json")
              .Build();
 
-            con = config.GetConnectionString("DB_KEY");
+            conString = config.GetConnectionString("DB_KEY");
         }
 
-        public bool HandleInformation(string userName, string passWord)
+        public string HandleInformation(string userName, string passWord)
         {
-            bool success = false; // false by default
-            //TODO: implement handling of information where we specifically communicate with the database
+            string infoString = "";
 
-            return success;
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+
+            }
+                //TODO: implement handling of information where we specifically communicate with the database
+
+                return infoString;
         }
     }
 }
