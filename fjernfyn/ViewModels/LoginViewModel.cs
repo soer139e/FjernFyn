8c335871a1770163a0452c385fb9a31a71c72a98
averyden <igo408gt@gmail.com>
2025-02-11 12:26:03 +0100

    Create a new instance of the command handler, and assign it to a currently empty method

--- fjernfyn/ViewModels/LoginViewModel.cs
+++ fjernfyn/ViewModels/LoginViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace fjernfyn
+﻿using System.Windows.Input;
+
+namespace fjernfyn
 {
     public class LoginViewModel
     {
+        public ICommand loginCommand { get; }
+
+        public LoginViewModel() 
+        {
+            loginCommand = new CommandHandler(OnLoginClicked);
+        }
+
+        private void OnLoginClicked() { }
+
+
     }
 }
