5041a090f876552f8bedce17fe7c85248bfc6cc1
averyden <igo408gt@gmail.com>
2025-02-11 12:21:02 +0100

    update namespace name

--- fjernfyn/App.xaml.cs
+++ fjernfyn/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace fjernfynDesigns
+namespace fjernfyn
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ fjernfyn/handlers/CommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace fjernfyn
+{
+    public class CommandHandler : ICommand
+    {
+        private Action _execute;
+        public CommandHandler(Action command)
+        {
+            _execute = command;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+    }
+}
