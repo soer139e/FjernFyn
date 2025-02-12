fa7c9e54bae2b634e362427abf3db4ae45246af6
averyden <igo408gt@gmail.com>
2025-02-12 08:03:51 +0100

    Create the employee class

--- fjernfyn/Classes/Employee.cs
+++ fjernfyn/Classes/Employee.cs
@@ -2,6 +2,18 @@
 {
     public class Employee
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email {  get; set; }
+        public Department Department { get; set; }
+
+        public Employee(string username, string password, string mail, Department dep) 
+        {
+            Username = username;
+            Password = password;
+            Email = mail;
+            Department = dep;
+        }
 
     }
 }
