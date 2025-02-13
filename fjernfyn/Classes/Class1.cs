77936655b71035465306d549580b6ff022dd4e34
Averyden <igo408gt@gmail.com>
2025-02-13 11:12:15 +0100

    FOrgot what i did

---
+++ fjernfyn/Classes/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace fjernfyn
+{
+    public class EmptyStringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(value?.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+}
--- fjernfyn/Windows/FeedbackCreationWindow.xaml.cs
+++ fjernfyn/Windows/FeedbackCreationWindow.xaml.cs
@@ -6,24 +6,17 @@ namespace fjernfyn
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FeedbackCreationWindow : Window
     {
-        LoginViewModel viewModel = new LoginViewModel();
-        public MainWindow()
+        FeedbackCreateViewModel viewModel = new FeedbackCreateViewModel();
+
+        public FeedbackCreationWindow()
         {
             InitializeComponent();
             DataContext = viewModel;
         }
 
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            var passwordBox = sender as PasswordBox;
-            if (passwordBox != null)
-            {
-                var viewModel = (LoginViewModel)this.DataContext;
-                viewModel.Password = passwordBox.Password;
-            }
-        }
+       
 
     }
 }
