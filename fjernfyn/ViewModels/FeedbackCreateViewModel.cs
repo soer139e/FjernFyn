7666f3828b06fa71897883115451251a854498cd
Averyden <igo408gt@gmail.com>
2025-02-13 12:24:28 +0100

    Set up properties for databinding the user

--- fjernfyn/ViewModels/FeedbackCreateViewModel.cs
+++ fjernfyn/ViewModels/FeedbackCreateViewModel.cs
@@ -1,14 +1,54 @@
-﻿namespace fjernfyn
+﻿using System.ComponentModel;
+
+namespace fjernfyn
 {
-    public class FeedbackCreateViewModel
+    public class FeedbackCreateViewModel : INotifyPropertyChanged
     {
+
+        private string _employeeName;
+
+        public string EmployeeName
+        {
+            get { return _employeeName; }
+            set { _employeeName = value; OnPropertyChanged(nameof(EmployeeName)); }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; OnPropertyChanged(nameof(Email));}
+        }
+
+        private string _department;
+        
+        public string Department
+        {
+            get { return _department; }
+            set { _department = value; OnPropertyChanged(nameof(Department));}
+        }
 
         private string _feedbackContent;
 
-        public string FeedbackContent; 
+        public string FeedbackContent;
+
+
         /// <summary>
         /// TOOD: Implement INotifyPropertyChanged whenever we get to that.
         /// </summary>
         public FeedbackCreateViewModel() { }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var value = this.GetType().GetProperty(propertyName)?.GetValue(this, null);
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
