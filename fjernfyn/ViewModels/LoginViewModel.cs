b8af3d3cc9c9d005a400136f74ddbb5bf25a6626
Averyden <igo408gt@gmail.com>
2025-02-13 12:45:56 +0100

    Update code in loginviewmodel to be more cohesive and clean

--- fjernfyn/ViewModels/LoginViewModel.cs
+++ fjernfyn/ViewModels/LoginViewModel.cs
@@ -7,80 +7,46 @@ namespace fjernfyn
 {
     public class LoginViewModel : INotifyPropertyChanged
     {
-        private readonly string con;
+        private readonly EmployeeRepository _employeeRepository;
 
-        private EmployeeRepository empRepo = new EmployeeRepository();
-
-        
-        public ICommand loginCommand { get; }
-
-        private string _userName;
-
-        private string _password;
-
-        private Employee _selectedEmp;
-
-        public Employee SelectedEmp
+        public LoginViewModel()
         {
-            get { return _selectedEmp; }
-            set { _selectedEmp = value; OnPropertyChanged(nameof(SelectedEmp)); }
+            _employeeRepository = new EmployeeRepository();
         }
 
-        public string UserName
+        public ICommand LoginCommand => new CommandHandler(OnLoginClicked);
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnLoginClicked()
         {
-            get { return _userName; }
-            set { _userName = value; OnPropertyChanged(nameof(UserName)); }
+            var employee = Login();
+
+            if (employee != null)
+            {
+                MessageBox.Show($"Velkommen: {employee.Username} ({employee.Email})\n\n\nHusk at være grundig i din feedback.", "Logget ind");
+            }
+            else
+            {
+                MessageBox.Show("Der skete en fejl under login (6x624)", "Fejl");
+            }
         }
 
-        public string Password
+        public Employee Login()
         {
-            get { return _password; }
-            set { _password = value; OnPropertyChanged(nameof(Password)); }
-        }
-
-        public LoginViewModel() 
-        {
-            loginCommand = new CommandHandler(OnLoginClicked);
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-
-        /// <summary>
-        /// For some weird reason, changing the return type of this bound command to employee, results in everything going wrong.
-        /// So we need to have this OnLoginClicked method, which then instantly calls the SendInformation method.
-        /// Since binding to a command with a return type isnt allowed since its of an Action datatype... thank you microsoft.
-        /// </summary>
-        private void OnLoginClicked()  
-        {
-            sendInformation();
-        }
-
-        private Employee sendInformation()
-        {
-            string userInfo = empRepo.HandleInformation(UserName, Password);
+            string userInfo = _employeeRepository.HandleInformation(UserName, Password);
             string[] splitString = userInfo.Split("|");
-            Employee finalEmp = null;
 
             if (splitString.Length > 1)
             {
-                finalEmp = new Employee(splitString[0], splitString[1], splitString[2], (Department)Enum.Parse(typeof(Department), splitString[3]));
-                MessageBox.Show($"Velkommen: {splitString[4]} ({splitString[2]})\n\n\nHusk at være grundig i din feedback.", "Logget ind");
-            } else
-            {
-                MessageBox.Show(userInfo, "Fejl");
+                Employee employee = new Employee(splitString[0], splitString[1], splitString[2], (Department)Enum.Parse(typeof(Department), splitString[3]));
+                return employee;
             }
-            return finalEmp;
-        }
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            var value = this.GetType().GetProperty(propertyName)?.GetValue(this, null);
-            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
-            if (propertyChanged != null)
-            {
-                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            return null;
         }
     }
+
 }
