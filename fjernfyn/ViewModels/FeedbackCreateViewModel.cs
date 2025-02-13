e5dfc52cde0a8f51c64fe4a3eb0188e2a315be57
Averyden <igo408gt@gmail.com>
2025-02-13 09:45:29 +0100

    Create properties for viewmodel

--- fjernfyn/ViewModels/FeedbackCreateViewModel.cs
+++ fjernfyn/ViewModels/FeedbackCreateViewModel.cs
@@ -2,6 +2,13 @@
 {
     public class FeedbackCreateViewModel
     {
+
+        private string _feedbackContent;
+
+        public string FeedbackContent; 
+        /// <summary>
+        /// TOOD: Implement INotifyPropertyChanged whenever we get to that.
+        /// </summary>
         public FeedbackCreateViewModel() { }
     }
 }
