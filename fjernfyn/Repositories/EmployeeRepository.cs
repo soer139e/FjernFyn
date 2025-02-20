19619007927959b1d225c1de6a22db9ea6db2c47
Averyden <igo408gt@gmail.com>
2025-02-20 14:20:52 +0100

    rewrite HandleInformation method

--- fjernfyn/Repositories/EmployeeRepository.cs
+++ fjernfyn/Repositories/EmployeeRepository.cs
@@ -59,71 +59,89 @@ namespace fjernfyn.Repositories
         //}
 
         // TODO: rewrite to find a singular row using a singular query, then run another one, to fetch the rest of the info.
-        public string HandleInformation(Employee emp)
+        public bool HandleInformation(Employee emp)
         {
             string infoString = "";
+
+            bool valid = false;
 
             using (SqlConnection con = new SqlConnection(conString))
             {
                 con.Open();
 
-                using (SqlCommand cmd = new SqlCommand("SELECT userName FROM Employees WHERE userName = @userName", con))
+                string query = "SELECT * FROM Employees WHERE userName = @userName AND passWord = @passWord;";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
                 {
-                    cmd.Parameters.Add("@userName", SqlDbType.NVarChar).Value = userName;
+                    cmd.Parameters.AddWithValue("@userName", emp.Username);
+                    cmd.Parameters.AddWithValue("@passWord", emp.Password);
 
-                    object result = cmd.ExecuteScalar();
-
-                    if (result != null)
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        Debug.WriteLine($"Employee found: {result.ToString()}");
-                        // NESTED USING DIRECTIVES LETS FUCKING GOOOOOOO!!!!
-
-                        using (SqlCommand cmdPass = new SqlCommand("SELECT passWord FROM Employees WHERE userName = @userName", con))
+                        if (reader.HasRows)
                         {
-                            cmdPass.Parameters.Add("@userName", SqlDbType.NVarChar).Value = userName;
+                            valid = true;
 
-                            object result2 = cmdPass.ExecuteScalar();
-                            Debug.WriteLine($"{result2.ToString()}");
-                            if (result2.ToString() == passWord)
-                            {
-                                Debug.WriteLine("Password is valid when matched with database");
-                                using (SqlCommand fetchAll = new SqlCommand("SELECT email, department, Name FROM Employees WHERE userName = @userName", con))
-                                {
-                                    fetchAll.Parameters.Add("@userName", SqlDbType.NVarChar).Value = userName;
-
-                                    using (SqlDataReader reader = fetchAll.ExecuteReader())
-                                    {
-                                        if (reader.Read() && reader["email"].ToString() != "" && reader["department"].ToString() != "" && reader["Name"].ToString() != "") 
-                                        {
-                                            string email = reader["email"].ToString();
-                                            string department = reader["department"].ToString();
-                                            string name = reader["Name"].ToString();
-
-                                            Debug.WriteLine($"Email: {email}, Department: {department}, Name: {name}");
-                                            infoString = $"{userName}|{passWord}|{email}|{department}|{name}";
-                                            
-                                        }
-                                        else
-                                        {
-                                            infoString = "Der skete en fejl under login\n\nFejlkode: (6x548)";
-                                        }
-                                    }
-                                }
-                            } else
-                            {
-                                infoString = "Brugernavn eller kodeord er forkert.";
-                            }
+                            reader.Read();
+                            emp.Email = reader.GetString(3);
+                            //emp.Department = reader.GetEnumerator(4); // TODO: parse the enum
 
                         }
                     }
-                    else
-                    {
-                        infoString = $"Brugernavn eller kodeord er forkert.";
-                    }
+
+                    //object result = cmd.ExecuteScalar();
+
+                    //if (result != null)
+                    //{
+                    //    Debug.WriteLine($"Employee found: {result.ToString()}");
+                    //    // NESTED USING DIRECTIVES LETS FUCKING GOOOOOOO!!!!
+
+                    //    using (SqlCommand cmdPass = new SqlCommand("SELECT passWord FROM Employees WHERE userName = @userName", con))
+                    //    {
+                    //        cmdPass.Parameters.Add("@userName", SqlDbType.NVarChar).Value = userName;
+
+                    //        object result2 = cmdPass.ExecuteScalar();
+                    //        Debug.WriteLine($"{result2.ToString()}");
+                    //        if (result2.ToString() == passWord)
+                    //        {
+                    //            Debug.WriteLine("Password is valid when matched with database");
+                    //            using (SqlCommand fetchAll = new SqlCommand("SELECT email, department, Name FROM Employees WHERE userName = @userName", con))
+                    //            {
+                    //                fetchAll.Parameters.Add("@userName", SqlDbType.NVarChar).Value = userName;
+
+                    //                using (SqlDataReader reader = fetchAll.ExecuteReader())
+                    //                {
+                    //                    if (reader.Read() && reader["email"].ToString() != "" && reader["department"].ToString() != "" && reader["Name"].ToString() != "") 
+                    //                    {
+                    //                        string email = reader["email"].ToString();
+                    //                        string department = reader["department"].ToString();
+                    //                        string name = reader["Name"].ToString();
+
+                    //                        Debug.WriteLine($"Email: {email}, Department: {department}, Name: {name}");
+                    //                        infoString = $"{userName}|{passWord}|{email}|{department}|{name}";
+
+                    //                    }
+                    //                    else
+                    //                    {
+                    //                        infoString = "Der skete en fejl under login\n\nFejlkode: (6x548)";
+                    //                    }
+                    //                }
+                    //            }
+                    //        } else
+                    //        {
+                    //            infoString = "Brugernavn eller kodeord er forkert.";
+                    //        }
+
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    infoString = $"Brugernavn eller kodeord er forkert.";
+                    //}
                 }
 
             }
-            return infoString;
+            return valid;
         }
     }
 }
