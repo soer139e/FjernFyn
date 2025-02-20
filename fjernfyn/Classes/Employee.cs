5891c353f39888e52b21a2c9fbdce8d8d267a985
Averyden <igo408gt@gmail.com>
2025-02-20 14:26:21 +0100

    remove constructors and add the full name as a property

--- fjernfyn/Classes/Employee.cs
+++ fjernfyn/Classes/Employee.cs
@@ -7,20 +7,9 @@
         public string Email {  get; set; }
         public Department Department { get; set; }
 
+        public string FullName {  get; set; }
+
         //TODO: constructor chain, so that the user can only input the username and password, and get away with it.
-
-        //public Employee(string username, string password, string mail, Department dep) 
-        //{
-        //    Username = username;
-        //    Password = password;
-        //    Email = mail;
-        //    Department = dep;
-        //}
-
-        public Employee() 
-        {
-
-        }
 
     }
 }
