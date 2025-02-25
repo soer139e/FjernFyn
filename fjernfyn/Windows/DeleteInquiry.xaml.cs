a0e35e7318bad0ca009f21d98e29f3daf55536f7
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-02-25 13:08:26 +0100

    wireframe for deleting

---
+++ fjernfyn/Windows/DeleteInquiry.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fjernfyn.Windows
+{
+    /// <summary>
+    /// Interaction logic for DeleteInquiry.xaml
+    /// </summary>
+    public partial class DeleteInquiry : Window
+    {
+        public DeleteInquiry()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
