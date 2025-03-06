550a285e20fbdebee112da4c522688e6e25a96bf
Søren <Soren.ploug04@gmail.com>
2025-03-06 13:28:44 +0100

    Test af Byte[] til Image

--- fjernfyn/ViewModels/ReadInquiryviewModel.cs
+++ fjernfyn/ViewModels/ReadInquiryviewModel.cs
@@ -12,11 +12,30 @@ namespace fjernfyn.ViewModels
 {
     public class ReadInquiryViewModel
     {
+       public Image Display { get; set; }
        public  Feedback Inquiry {  get; set; }
         public ReadInquiryViewModel(Feedback inquiry)
         { 
             Inquiry = inquiry;
+            LoadImage(Inquiry.Image);
         }
 
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+
+        }
     }
-}
