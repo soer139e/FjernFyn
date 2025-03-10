4bb5b79183df4928929a1987d9786a9b9cdfcd68
Søren Frederik Ploug <soren.ploug04@gmail.com>
2025-03-10 12:26:34 +0100

    navnav

--- fjernfyn/ViewModels/AdmindashBoardViewModel.cs
+++ fjernfyn/ViewModels/AdmindashBoardViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using fjernfyn.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace fjernfyn.ViewModels
 {
     public class AdmindashBoardViewModel
     {
+        public ICommand InquiryOverViewCommand { get; }
+        public AdminDashBoardWindow window { get; set; }
+
+        public AdmindashBoardViewModel()
+        {
+            InquiryOverViewCommand = new CommandHandler(OpenInquiryOverview);
+        }
+
+        public void OpenInquiryOverview()
+        {
+
+        }
     }
 }
