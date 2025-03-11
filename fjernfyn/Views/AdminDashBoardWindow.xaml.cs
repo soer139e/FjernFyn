f2d09abcd4618809a687977dd25f9cdbce085a78
Marcus <marcuskristensen10@hotmail.com>
2025-03-11 11:34:40 +0100

    klargjort til programmering af metoder i adminviewmodel

--- fjernfyn/Views/AdminDashBoardWindow.xaml.cs
+++ fjernfyn/Views/AdminDashBoardWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using fjernfyn.ViewModels;
 
 namespace fjernfyn.Views
 {
@@ -20,9 +21,12 @@ namespace fjernfyn.Views
     public partial class AdminDashBoardWindow : Window
     {
         private Employee _employee { get; set; }
+        public AdminDashBoardViewModel adbvw { get; set; }
+ 
         public AdminDashBoardWindow(Employee employee)
         {
             InitializeComponent();
+            adbvw = new AdminDashBoardViewModel(this, _employee);
             _employee = employee;
         }
     }
