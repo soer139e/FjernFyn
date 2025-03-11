d0cae73e427e5ee2bfbc3a7a245fd8bc50c28cdb
Marcus <marcuskristensen10@hotmail.com>
2025-03-11 11:51:14 +0100

    Pølse

--- fjernfyn/ViewModels/AdmindashBoardViewModel.cs
+++ fjernfyn/ViewModels/AdmindashBoardViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using fjernfyn.Windows;
 
 namespace fjernfyn.ViewModels
 {
@@ -29,32 +30,29 @@ namespace fjernfyn.ViewModels
         public void CreateFeedbackClicked()
         {
 
-            //Window.Hide();
-            //CreateFeedbackWindow createFeedbackWindow = new CreateFeedbackWindow(employee);
-            //createFeedbackWindow.Show();
+            
+            FeedbackCreationWindow createFeedbackWindow = new FeedbackCreationWindow(employee);
+            createFeedbackWindow.Show();
+            Window.Close();
         }
 
         public void ShowFeedbackClicked()
         {
 
-            //Window.Hide();
-            //InquiryOverviewWindow inquiryOverviewWindow = new InquiryOverviewWindow(employee);
-            //inquiryOverviewWindow.Show();
+         
+            InquiryOverview inquiryOverviewWindow = new InquiryOverview();
+            inquiryOverviewWindow.Show();
+            Window.Close();
         }
 
         public void ShowMarkedDoneClicked()
         {
 
-            //Window.Hide();
-            //MarkedDoneWindow markedDoneWindow = new MarkedDoneWindow(employee);
-            //markedDoneWindow.Show();
         }
 
         public void AdminPanelClicked()
         {
-            //Window.Hide();
-            //AdminPanelWindow adminPanelWindow = new AdminPanelWindow(employee);
-            //adminPanelWindow.Show();
+
         }
     }
 }
