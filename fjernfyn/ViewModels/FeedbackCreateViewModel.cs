aa4569621bf84f6d2e633821e34a25ffd81b9917
Avery <30341467+Averyden@users.noreply.github.com>
2025-03-17 19:14:30 +0100

    Revert commit
    Look into this later, as this is a public property, wed need to change other bits of the code to make it work again... woops

--- fjernfyn/ViewModels/FeedbackCreateViewModel.cs
+++ fjernfyn/ViewModels/FeedbackCreateViewModel.cs
@@ -27,7 +27,7 @@ namespace fjernfyn
 
         public Employee Employee { get; set; }
 
-        public ICommand SendCommand { get; }
+        public ICommand sendCommand { get; }
         //public ICommand addSoftwareCommand { get; }
         public ICommand AddErrorImageCommand { get; }
 
@@ -73,7 +73,7 @@ namespace fjernfyn
 
             AddErrorImageCommand = new CommandHandler(AddErrorImage);
             Softwares = softwareRepo.GetAll();
-            SendCommand = new CommandHandler(SendClicked);
+            sendCommand = new CommandHandler(SendClicked);
             //addSoftwareCommand = new CommandHandler(PlusClicked);
             //TODO: We need to either
             // A. remove the explicit Feedback constructor
